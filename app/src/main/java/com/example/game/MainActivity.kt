c27e44eb7f74ff6773c9ed1dc9a98be71986033c
Juan Pablo Bautista <jbautistaclavijo@gmail.com>
2024-05-14 22:16:31 -0500

    iniciando proyecto y estableciendo logica para el manejo de la matriz - post data usando Composable para vistas

---
+++ app/src/main/java/com/example/game/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.taller22048game
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taller22048game.ui.theme.Taller22048GameTheme
+
+class MainActivity() : ComponentActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
+
+
+
